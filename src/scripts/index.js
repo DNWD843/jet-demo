39d4cce34d89e0bb2c2d41489138e1819e902dbc
dneklyudov <dneklyudov@i-novus.ru>
2023-02-13 00:03:09 +0300

    added background color for main and button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -151,13 +151,42 @@ const Header = Jet.createClass({
 Jet.render(Jet.createElement(Header, { title: 'Jet', subTitle: 'Just try it!' }), headerContainer);
 
 const Main = Jet.createClass({
+  getInitialState() {
+    return ({ color: 'red' })
+  },
+
+  componentWillMount() {
+    this.handleClick = this.handleClick.bind(this);
+  },
+
+  componentDidMount() {
+    console.log('yo', this.state.color)
+  },
+
+  handleClick() {
+    console.log('yo click', this)
+    this.setState(prev => {
+      console.log('yo prev', prev)
+      if (prev.color === 'red') {
+        return ({ color: 'green' })
+      }
+
+      if (prev.color === 'green') {
+        return ({ color: 'blue' })
+      }
+
+      return ({ color: 'red' });
+    })
+  },
+
   render() {
     return Jet.createElement(
       'div',
-      { className: 'main-container' },
+      { className: `main-container color-${this.state.color}` },
       [
         Jet.createElement('span', { className: 'content-title' }, this.props.contentTitle || ''),
         Jet.createElement('div', { className: 'content-image' }),
+        Jet.createElement('button', { className: 'content-color-button', onclick: this.handleClick }, 'Click me'),
       ],
     );
   }
